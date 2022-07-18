2b7fbb7e87c8fadc446faac4759fe48e4dd320e1
seunoduez@gmail.com <seunoduez@gmail.com>
2022-07-18 16:34:17 +0100

    Add sms job to send invoice.

--- job/reminder.js
+++ job/reminder.js
@@ -2,20 +2,6 @@ const cron = require('node-cron');
 const SendEmail = require('../config/emailConfig')
 const Payment = require('./paystackPayment')
 
-
-// async function startEndReminderCronJob (invoice, payment_link, user_id) {
-//   const reminder_invoice_job = cron.schedule('*/20 * * * * *', async () => {
-//     if (await checkIfInvoiceIsPaid(invoice.id, user_id)) {
-//       endReminderCronJob(reminder_invoice_job)
-//     }
-//     else {
-//       await SendEmail.sendReminder(invoice, payment_link)
-//       console.log(`Email sent to ${invoice.email}`)
-//     }  
-//   });
-// }
-
-
 async function startEndReminderCronJob (invoice, payment_link, reference) {
   const reminder_invoice_job = cron.schedule('*/20 * * * * *', async () => {
     const response = await Payment.verifyPayment(reference)
@@ -25,7 +11,6 @@ async function startEndReminderCronJob (invoice, payment_link, reference) {
     } 
     else {
       await SendEmail.sendReminder(invoice, payment_link)
-      // console.log(`Email sent to ${invoice.email}`)
     }  
   });
 }
